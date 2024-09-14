d6ec6313f21dd832277cd8383179582ae2f677ac
AbanoubSaweris <abanoubphelopos12@gmail.com>
2024-09-14 14:00:26 +0200

    add parameter Binding

--- Survey.Api/Controllers/PollsController.cs
+++ Survey.Api/Controllers/PollsController.cs
@@ -12,21 +12,21 @@ public class PollsController(IPollServices services) : BaseApiController
         return Ok(pollResponse);
     }
     [HttpGet("{id}")]
-    public IActionResult Get(Guid id)
+    public IActionResult Get([FromRoute] Guid id)
     {
         var poll = services.Get(id);
         return poll is null ? NotFound() : Ok(poll.MapToResponse());
     }
 
     [HttpPost("")]
-    public IActionResult Add(CreatePollRequest request)
+    public IActionResult Add([FromBody] CreatePollRequest request)
     {
         var newRequest = services.Add(request.MapToPoll());
         return CreatedAtAction(nameof(Get), new {id = newRequest.Id}, newRequest);
     }
 
     [HttpPut("{id}")]
-    public IActionResult Update(Guid id, UpdatePollRequest request)
+    public IActionResult Update([FromRoute] Guid id, [FromBody] UpdatePollRequest request)
     {
         var isUpdated = services.Update(id, request.MapToPoll(id));
         if (!isUpdated)
@@ -35,7 +35,7 @@ public class PollsController(IPollServices services) : BaseApiController
     }
 
     [HttpDelete("{id}")]
-    public IActionResult Delete(Guid id)
+    public IActionResult Delete([FromRoute] Guid id)
     {
         var sDeleted = services.Delete(id);
         if (!sDeleted)
