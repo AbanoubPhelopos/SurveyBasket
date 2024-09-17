4afeda517308cd5191ff61ec687d4d99fe41f112
AbanoubSaweris <abanoubphelopos12@gmail.com>
2024-09-17 18:25:10 +0200

    add ICollectionServices class

---
+++ Survey.Api/ApplicationServiceCollectionExtention.cs
@@ -0,0 +1,28 @@
+﻿namespace Survey.Api
+{
+    public static class ApplicationServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddControllers();
+            
+            // Properly chaining Swagger and other services
+            services.AddEndpointsApiExplorer()
+                .AddSwaggerGen()
+                .AddApplicationDependencies();
+            
+            return services;
+        }
+
+        private static IServiceCollection AddApplicationDependencies(this IServiceCollection services)
+        {
+            // Registering services and FluentValidation dependencies
+            services.AddScoped<IPollServices, PollServices>();
+
+            services.AddFluentValidationAutoValidation()  // Ensure FluentValidation setup
+                .AddValidatorsFromAssembly(typeof(CreatePollRequestValidation).Assembly);
+            
+            return services;
+        }
+    }
+}
--- Survey.Api/GlobalUsings.cs
+++ Survey.Api/GlobalUsings.cs
@@ -1,3 +1,8 @@
 ﻿global using Microsoft.AspNetCore.Mvc;
 global using Survey.Application.Models;
 global using Survey.Application.Services;
+
+global using Survey.Api;
+global using FluentValidation;
+global using FluentValidation.AspNetCore;
+global using Survey.Contract.Requests.RequestsValidations;
